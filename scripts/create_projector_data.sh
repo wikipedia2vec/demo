098f6c87ba195f9ad014ff8ffce8bcc13c409847
Ikuya Yamada <ikuya@ikuya.net>
2020-01-15 00:40:08 +0900

    add embedding data

--- scripts/create_projector_data.sh
+++ scripts/create_projector_data.sh
@@ -3,10 +3,18 @@ export LANG=C.UTF-8
 
 echo "{"
 echo '  "embeddings": ['
-for LANG in "ar" "de" "en" "es" "fr" "it" "ja" "nl" "pl" "pt" "ru" "zh"
+for LANG in "en" "ar" "de" "es" "fr" "it" "ja" "nl" "pl" "pt" "ru" "zh"
     do
-        python create_projector_data.py ${LANG}wiki_20180420_100d.pkl --tensor-file=${LANG}_emb.tsv --metadata-file=${LANG}_data.tsv --config-file=${LANG}_config.json --base-url=http://wikipedia2vec.github.io/demo/projector_files/
-        sed -e '1,2d' -e '12,13d'  -e "11s/$/,/" < ${LANG}_config.json
+        python create_projector_data.py ${LANG}wiki_20180420_100d.pkl --tensor-file=${LANG}_emb.tsv --metadata-file=${LANG}_data.tsv --config-file=${LANG}_config.json --base-url=https://wikipedia2vec.github.io/demo/projector_files/
+        if [ $LANG = "zh" ]; then
+            sed -e '1,2d' -e '12,13d' < ${LANG}_config.json
+        else
+            sed -e '1,2d' -e '12,13d' -e "11s/$/,/" < ${LANG}_config.json
+        fi
+        if [ $LANG = "en" ]; then
+            python create_projector_data.py ${LANG}wiki_20180420_nolg_100d.pkl --tensor-file=${LANG}_nolg_emb.tsv --metadata-file=${LANG}_nolg_data.tsv --config-file=${LANG}_nolg_config.json --model-name="English (w/o link graph)" --base-url=https://wikipedia2vec.github.io/demo/projector_files/
+            sed -e '1,2d' -e '12,13d'  -e "11s/$/,/" < ${LANG}_nolg_config.json
+        fi
     done
 echo "  ]"
 echo "}"
--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -2,3 +2,4 @@ for LANG in "ar" "de" "en" "es" "fr" "it" "ja" "nl" "pl" "pt" "ru" "zh"
     do
         curl -o - https://wikipedia2vec.s3-ap-northeast-1.amazonaws.com/models/$LANG/2018-04-20/${LANG}wiki_20180420_100d.pkl.bz2 | bunzip2 > ${LANG}wiki_20180420_100d.pkl
     done
+curl -o - https://wikipedia2vec.s3-ap-northeast-1.amazonaws.com/models/en/2018-04-20/enwiki_20180420_nolg_100d.pkl.bz2 | bunzip2 > enwiki_20180420_nolg_100d.pkl
